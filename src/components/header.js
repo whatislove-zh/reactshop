8159e343991f86b7360799fed0847f1da681fe49
Vladislav <vladislavfrontdev@gmail.com>
2022-09-23 16:26:53 +0300

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -15,24 +15,48 @@ const showOrder = (props) => {
   )
 }
 
+const showContact = () => {
+  return (
+    <div className='contacts'>
+      <p>Контактний номер: <a href='tel: +380996863705' target="_self">+380996863705</a></p>
+      <p>Email: <span>vladislavfrontdev@gmail.com</span></p>
+      <p><a href='https://github.com/whatislove-zh/reactshop'>Наш репозиторій у github</a></p>
+      <p><a href='https://t.me/vladFreelance'>Натисніть щоб написати нам через Telegram</a></p>
+    </div>
+  )
+}
+
 const showNothing = () => {
   return(<div className='empty'>Кошик пустий =(</div>)
+}
+const showLogin = () => {
+  return(<div className='timeLimitInfo'><p>Нажаль поки що кабінет недоступний.</p></div>)
+}
+const showAbout = () => {
+  return(<div className='timeLimitInfo about'><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></div>)
 }
 
 export default function Header(props) {
   let [cardOpen, setCardOpen] = useState(false)
+  let [contactOpen, setContactOpen] = useState(false)
+  let [loginOpen, setLoginOpen] = useState(false)
+  let [aboutOpen, setAboutOpen] = useState(false)
 
   return (<header>
     <span className='logo'>Green Shop</span>
     <ul className='nav'>
-        <li>Про нас</li>
-        <li>Контакти</li>
-        <li>Кабінет</li>
+        <li onClick={() => {setAboutOpen(aboutOpen = !aboutOpen)}}>Про нас</li>
+        <li onClick={() => {setContactOpen(contactOpen = !contactOpen)}}>Контакти</li>
+        <li onClick={() => {setLoginOpen(loginOpen = !loginOpen)}}>Кабінет</li>
         
     </ul>
     <FiShoppingCart onClick={() => {setCardOpen(cardOpen = !cardOpen)}} className={`shop-card-button ${cardOpen && 'active'}`}/>
     
-    {cardOpen && (<div className={`shop-card ${cardOpen && "animation"}`}>
+    {contactOpen && showContact()}
+    {loginOpen && showLogin()}
+    {aboutOpen && showAbout()}
+
+    {cardOpen && (<div className='shop-card'>
       {props.orders.length > 0 ? showOrder(props) : showNothing()}
     </div>)}
 
