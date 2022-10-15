f7a952605ae31396041eab7dd50abe45daebed9b
Vladislav <vladislavfrontdev@gmail.com>
2022-10-15 20:07:22 +0300

    add alt for img

--- src/components/order.js
+++ src/components/order.js
@@ -5,7 +5,7 @@ export class Order extends Component {
   render() {
     return (
       <div className='item'>
-        <img src={"./img/"+ this.props.item.img} />
+        <img src={"./img/"+ this.props.item.img} alt= {this.props.item.title} />
         <h2>{this.props.item.title}</h2>
         <b>{this.props.item.price} $</b>
         <FiTrash className='delete-icon' onClick={() => {this.props.onDelete(this.props.item.id)}} />
